d1218ede6ad923e38b90b9a0225f23215e1b7df1
Juan Gamiño <gamino_juan03@hotmail.com>
2020-08-21 17:17:47 -0500

    cambios estructura

--- lib/src/screens/routes/rutas.dart
+++ lib/src/screens/routes/rutas.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/cupertino.dart';
 
-import 'package:final_flutter_app/src/screens/actividad_uno.dart';
+import 'package:final_flutter_app/src/screens/material/actividad_uno.dart';
 import 'package:final_flutter_app/src/screens/home_screen.dart';
-import 'package:final_flutter_app/src/screens/tareas.dart';
-import 'package:final_flutter_app/src/screens/docuemntos.dart';
-import 'package:final_flutter_app/src/screens/lista.dart';
+import 'package:final_flutter_app/src/screens/material/tareas.dart';
+import 'package:final_flutter_app/src/screens/material/docuemntos.dart';
+import 'package:final_flutter_app/src/screens/material/lista.dart';
 
 Map<String, WidgetBuilder> getAplicationRouts() {
   return <String, WidgetBuilder>{
